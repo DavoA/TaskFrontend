f11c05681dcad8116882545cfe6ba75fe5d6488a
Ваше Имя <you@example.com>
2024-03-21 16:07:44 +0400

    Added bash script

---
+++ changing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOCKER_COMPOSE_FILE=$(dirname "$0")/docker-compose.yml
+
+current_tag=$(grep 'parandzem/front' $DOCKER_COMPOSE_FILE | awk -F ':' '{print $3}' | tr -d '"')
+
+if [ -z "$current_tag" ]; then
+    echo "Could not find the tag for 'parandzem/front' image in the Docker Compose file."
+    exit 1
+fi
+
+new_tag="latest"
+
+sed -i "s/parandzem\/front:$current_tag/parandzem\/front:$new_tag/g" $DOCKER_COMPOSE_FILE
+
+echo "Tag for 'parandzem/front' image in the Docker Compose file has been updated from $current_tag to $new_tag."
